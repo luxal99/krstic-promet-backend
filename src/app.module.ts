b00fb257f213ffbaba487119d9dea35964fc7832
Aleksa Lukic <lukicaleksa04@gmail.com>
2021-11-08 11:58:54 +0100

    Register user and auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import {Module} from "@nestjs/common";
 import {AppController} from "./app.controller";
 import {AppService} from "./app.service";
 import {CONFIG, DATABASE_CONFIG} from "./constant/constant";
+import {UserModule} from "./resources/user/user.module";
+import {ArticleModule} from "./resources/article/article.module";
+import {ArticleSubCategoryModule} from "./resources/article-sub-category/article-sub-category.module";
+import {ArticleCategoryModule} from "./resources/article-category/article-category.module";
+import {ConversionModule} from "./resources/conversion/conversion.module";
+import {WarehouseModule} from "./resources/warehouse/warehouse.module";
 
 @Module({
-    imports: [CONFIG, DATABASE_CONFIG],
+    imports: [
+        CONFIG, DATABASE_CONFIG,
+        ArticleModule, ArticleSubCategoryModule,
+        ArticleCategoryModule, ConversionModule,
+        WarehouseModule, UserModule
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
--- src/resources/user/user.module.ts
+++ src/resources/user/user.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
+import {Module} from "@nestjs/common";
+import {UserService} from "./user.service";
+import {UserController} from "./user.controller";
+import {UserRepository} from "../../repository/UserRepository";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  controllers: [UserController],
-  providers: [UserService]
+    imports: [TypeOrmModule.forFeature([UserRepository])],
+    controllers: [UserController],
+    providers: [UserService]
 })
-export class UserModule {}
+export class UserModule {
+}
