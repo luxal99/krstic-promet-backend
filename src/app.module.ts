3f9ee6869618a391be6230e1cabf94c91af1d73f
Aleksa Lukic <lukicaleksa04@gmail.com>
2022-01-17 10:31:22 +0100

    Client CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,38 +1,44 @@
-import {MiddlewareConsumer, Module, RequestMethod} from "@nestjs/common";
-import {AppController} from "./app.controller";
-import {AppService} from "./app.service";
-import {CONFIG, DATABASE_CONFIG} from "./constant/constant";
-import {UserModule} from "./resources/user/user.module";
-import {ArticleModule} from "./resources/article/article.module";
-import {ArticleSubCategoryModule} from "./resources/article-sub-category/article-sub-category.module";
-import {ArticleCategoryModule} from "./resources/article-category/article-category.module";
-import {ConversionModule} from "./resources/conversion/conversion.module";
-import {WarehouseModule} from "./resources/warehouse/warehouse.module";
-import {JwtMiddleware} from "./middleware/jwt.middleware";
+import { MiddlewareConsumer, Module, RequestMethod } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { CONFIG, DATABASE_CONFIG } from "./constant/constant";
+import { UserModule } from "./resources/user/user.module";
+import { ArticleModule } from "./resources/article/article.module";
+import { ArticleSubCategoryModule } from "./resources/article-sub-category/article-sub-category.module";
+import { ArticleCategoryModule } from "./resources/article-category/article-category.module";
+import { ConversionModule } from "./resources/conversion/conversion.module";
+import { WarehouseModule } from "./resources/warehouse/warehouse.module";
+import { JwtMiddleware } from "./middleware/jwt.middleware";
+import { ClientModule } from "./resources/client/client.module";
 
 @Module({
-    imports: [
-        CONFIG, DATABASE_CONFIG,
-        ArticleModule, ArticleSubCategoryModule,
-        ArticleCategoryModule, ConversionModule,
-        WarehouseModule, UserModule
-    ],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    CONFIG,
+    DATABASE_CONFIG,
+    ArticleModule,
+    ArticleSubCategoryModule,
+    ArticleCategoryModule,
+    ConversionModule,
+    ClientModule,
+    WarehouseModule,
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {
-    configure(consumer: MiddlewareConsumer): any {
-        consumer
-            .apply(JwtMiddleware)
-            .exclude(
-                {path: "user/auth", method: RequestMethod.POST},
-                {path: "user", method: RequestMethod.POST},
-            )
-            .forRoutes(
-                {method: RequestMethod.POST, path: "*"},
-                {method: RequestMethod.DELETE, path: "*"},
-                {path: "*", method: RequestMethod.PUT},
-                {path: "*", method: RequestMethod.GET},
-            );
-    }
+  configure(consumer: MiddlewareConsumer): any {
+    consumer
+      .apply(JwtMiddleware)
+      .exclude(
+        { path: "user/auth", method: RequestMethod.POST },
+        { path: "user", method: RequestMethod.POST }
+      )
+      .forRoutes(
+        { method: RequestMethod.POST, path: "*" },
+        { method: RequestMethod.DELETE, path: "*" },
+        { path: "*", method: RequestMethod.PUT },
+        { path: "*", method: RequestMethod.GET }
+      );
+  }
 }
--- src/repository/ClientRepository.ts
+++ src/repository/ClientRepository.ts
@@ -1,4 +1,5 @@
-import { Repository } from "typeorm";
+import { EntityRepository, Repository } from "typeorm";
 import { Client } from "../entities/Client";
 
+@EntityRepository(Client)
 export class ClientRepository extends Repository<Client> {}
